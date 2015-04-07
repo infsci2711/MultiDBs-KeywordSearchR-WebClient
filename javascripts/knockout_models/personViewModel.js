2906960e8b0accecbe4358b7b3af66ec8ac15c79
wenzhang61 <wez61@pitt.edu>
2015-04-07 09:31:39 -0400

    build index

--- javascripts/knockout_models/personViewModel.js
+++ javascripts/knockout_models/personViewModel.js
@@ -1,11 +1,13 @@
-var restBaseUrl = "http://52.1.107.126:7654/"; //aws ip
+//var restBaseUrl = "http://52.1.107.126:7654/"; //aws ip
+var restBaseUrl = "http://localhost:7654/";
 
-function PersonViewModel(term, databaseName, tableName) {
+function PersonViewModel(term, databaseName, tableName, columnName) {
 	var self = this;
 	
 	self.term = ko.observable(term);
 	self.databaseName = ko.observable(databaseName);
 	self.tableName = ko.observable(tableName);
+	self.columnName = ko.observable(columnName);
 
 }
 
@@ -20,7 +22,7 @@ function PersonsViewModel() {
 
 	self.findAll = function() {
 		$.ajax({
-			url: restBaseUrl + "Person",
+			url: restBaseUrl + "Index",
 			type: 'GET',
 			dataType: 'json',
 			contentType: "application/json",
@@ -29,20 +31,22 @@ function PersonsViewModel() {
 				self.people.removeAll();
 
 				for (var i = 0; i < data.length; i++) {
-					var person = new PersonViewModel(data[i].term, data[i].databaseName, data[i].tableName);
+					var person = new PersonViewModel(data[i].term, data[i].databaseName, data[i].tableName, data[i].columnName);
                    
 					self.people.push(person);
 				}
 			},
 			error: function(data) {
-				alert("Something went wrong while getting persons list. Please try again.");
+				alert("Something went wrong while getting Index list. Please try again.");
 			}
 		});
 	};
 
 	self.findById = function() {
+		var q = document.getElementById('key').value;
+		var splited = q.split("+");
 		$.ajax({
-			url: restBaseUrl + "Person/",
+			url: restBaseUrl + "Index/"+q,
 			type: 'GET',
 			dataType: 'json',
 			contentType: "application/json",
@@ -51,13 +55,13 @@ function PersonsViewModel() {
 				self.people.removeAll();
 
 				for (var i = 0; i < data.length; i++) {
-					var person = new PersonViewModel(data[i].term, data[i].databaseName, data[i].tableName);
+					var person = new PersonViewModel(data[i].term, data[i].databaseName, data[i].tableName, data[i].columnName);
                    
 					self.people.push(person);
 				}
 			},
 			error: function(data) {
-				alert("Something went wrong while getting persons list. Please try again.");
+				alert("Something went wrong while getting Index list. Please try again.");
 			}
 		});
 	};
@@ -65,15 +69,15 @@ function PersonsViewModel() {
 
 	self.addPerson = function() {
 		$.ajax({
-			url: restBaseUrl + "Person",
+			url: restBaseUrl + "Index",
 			type: 'PUT',
 			data: ko.toJSON(self.newPerson()),
 			dataType: 'json',
 			contentType: "application/json",
 			crossDomain: true,
 			success: function(data) {
-				self.people.push(new PersonViewModel(data.term, data.databaseName, data.tableName));
-				self.newPerson(new PersonViewModel());
+				self.people.push(new PersonViewModel("test", "test", "test", "test"));
+				//self.newPerson(new PersonViewModel());
 			},
 			error: function(data) {
 				alert("Something went wrong while adding new person. Please try again.");
